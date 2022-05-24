0ce46cf31c60b12dc105005816f733ac8e37bc21
Davide <davide.patria@gmail.com>
2022-05-24 16:11:06 +0200

    retab and stay on old package_name method for now

--- lua/ros-nvim/package.lua
+++ lua/ros-nvim/package.lua
@@ -1,32 +1,33 @@
 local M = {}
 
 function M.get_current_package_name(path)
-    path = path or vim.fn.expand("%:p")
-    local pkg_name = vim.fn.system(
-    [[python3 -c "try: import rospkg; print(rospkg.get_package_name(pwd))
-except: print('no_ros_on_system')"
-    ]])
+  path = path or vim.fn.expand("%:p")
+    -- local pkg_name = vim.fn.system('python3 -c "try: import rospkg; print(rospkg.get_package_name( \''..path ..'\')) \nexcept: print('no_ros_on_system')"'),
+  local pkg_name = vim.fn.system('python3 -c "import rospkg;print(rospkg.get_package_name(\'' .. path .. '\'))"')
 
-    print(pkg_name)
-    -- clean up output
-    pkg_name, _ = string.gsub(pkg_name, "\r", "")
-    pkg_name, _ = string.gsub(pkg_name, "\n", "")
-    if pkg_name == "None" then
-        return
-    end
-    return pkg_name
+  print("pkg_name" .. pkg_name)
+  print("inside function" .. pkg_name)
+  -- clean up output
+  pkg_name, _ = string.gsub(pkg_name, "\r", "")
+  pkg_name, _ = string.gsub(pkg_name, "\n", "")
+  if pkg_name == "None" then
+    -- return nil when rospkg says tha path is not inside
+    -- a ros package
+    return
+  end
+  return pkg_name
 end
 
 function M.get_current_package_path()
-    local pkg_name = M.get_current_package_name()
-    if pkg_name == nil then
-        return
-    end
-    local pkg_path = vim.fn.system("rospack find " .. pkg_name)
-    -- clean up output
-    pkg_path, _ = string.gsub(pkg_path, "\r", "")
-    pkg_path, _ = string.gsub(pkg_path, "\n", "")
-    return pkg_path
+  local pkg_name = M.get_current_package_name()
+  if pkg_name == nil then
+    return
+  end
+  local pkg_path = vim.fn.system("rospack find " .. pkg_name)
+  -- clean up output
+  pkg_path, _ = string.gsub(pkg_path, "\r", "")
+  pkg_path, _ = string.gsub(pkg_path, "\n", "")
+  return pkg_path
 end
 
 return M
